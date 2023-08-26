e8ed06068020cd83a2b3e1c16227f0da6488a7a0
deeptoe <rakinfrhn@gmail.com>
2023-08-27 02:50:13 +0600

    env update

--- db/index.js
+++ db/index.js
@@ -1,10 +1,12 @@
+import { config } from "dotenv";
 import mysql2 from "mysql2";
+config();
 
 const connection = mysql2.createConnection({
-  host: "localhost",
-  user: "toe",
-  password: "deepdeep",
-  database: "bloodbank",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_MYSQL,
 });
 
 export default connection;
