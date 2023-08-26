108850228513ec733fb4dbabdc21bcf8c94a8920
deeptoe <rakinfrhn@gmail.com>
2023-08-27 02:29:51 +0600

    add gitignore

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 import mysql2 from "mysql2";
 
 const connection = mysql2.createConnection({
-  host: "localhost",
+  host: process.env.HOST_MYSQL,
   user: "toe",
   password: "deepdeep",
   database: "bloodbank",
